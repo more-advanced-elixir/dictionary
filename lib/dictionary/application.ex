8d028ecdbabffe290ff714d9773d955030c56f32
pragdave <dave@pragdave.me>
2018-01-29 13:26:13 -0600

    updated

--- lib/dictionary/application.ex
+++ lib/dictionary/application.ex
@@ -3,6 +3,9 @@ defmodule Dictionary.Application do
   use Application
 
   def start(_type, _args) do
+
+    remember_we_are_the_server()
+
     children = [
       Dictionary.WordList,
     ]
@@ -15,4 +18,11 @@ defmodule Dictionary.Application do
     Supervisor.start_link(children, options)
   end
 
+  def are_we_the_server?() do
+    Application.get_env(:hangman, :we_are_the_server) == "oh yes"
+  end
+
+  defp remember_we_are_the_server() do
+    :ok = Application.put_env(:hangman, :we_are_the_server, "oh yes")
+  end
 end
